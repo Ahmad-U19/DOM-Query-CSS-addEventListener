cf7ae3bc9a7d57722982180035f64881a91ba29e
Ahmad-U19 <ahmedoppal2592@gmail.com>
2024-12-23 20:27:47 +0500

    CSS selector-1

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var element;
 
-document.querySelectorAll(".services-container").style.color="black";
-element=document.querySelectorAll(".services-container");
+document.querySelectorAll("#hamm").style.color="black";
+
+element=document.querySelectorAll("#hamm");
 
 console.log(element);
